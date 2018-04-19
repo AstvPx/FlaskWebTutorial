cc00620470fb8ea2e5fb9159f0d1374975070202
Phoenix <astvpx@126.com>
2018-04-19 21:50:46 +0800

    Chapter 04, Tag: 4c

--- flasky.py
+++ flasky.py
@@ -1,8 +1,11 @@
-from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from flask_script import Manager
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7003ea257ef06a71'
@@ -10,6 +13,12 @@ app.config['SECRET_KEY'] = '7003ea257ef06a71'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 manager = Manager(app)
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -19,13 +28,16 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 if __name__ == '__main__':
