a30f3de8d8c1b691e59aabe24573629875153f32
Phoenix <astvpx@126.com>
2018-04-19 22:25:08 +0800

    Chapter 05, Tag: 5d

--- flasky.py
+++ flasky.py
@@ -12,7 +12,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '7003ea257ef06a71'
+app.config['SECRET_KEY'] = '7003e257ef06a71'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
